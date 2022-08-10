49f60a3d5f348c5937035a87a55a58313b41eb79
marsshan2018 <55718531+marsshan2018@users.noreply.github.com>
2022-08-10 15:08:39 -0700

    Update BuildDryRun.groovy

--- LocalPipelineLibrary/vars/BuildDryRun.groovy
+++ LocalPipelineLibrary/vars/BuildDryRun.groovy
@@ -1,6 +1,3 @@
 public class BuildDryRun {
-	//Change jobName to "all" if you want to enable DryRun for all build jobs
-	//Change jobName to a specific job name, (eg. "run-tests") to run dryRun individually
-	//Change jobName to "none" to disable any DryRun(s)
-	public static String jobName = 'all'
+	public static Boolean bDryRun = false
 }
