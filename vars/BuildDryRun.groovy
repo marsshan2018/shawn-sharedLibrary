2384cf4ba09e90a9a649572f5015fc033127bd59
marsshan2018 <55718531+marsshan2018@users.noreply.github.com>
2022-08-10 15:36:01 -0700

    Update and rename BuildDryRun2.groovy to BuildDryRun.groovy

--- vars/BuildDryRun.groovy
+++ vars/BuildDryRun.groovy
@@ -2,5 +2,5 @@ public class BuildDryRun {
 	//Change jobName to "all" if you want to enable DryRun for all build jobs
 	//Change jobName to a specific job name, (eg. "run-tests") to run dryRun individually
 	//Change jobName to "none" to disable any DryRun(s)
-	public static String jobName = 'all'
+	public static Boolean bDryRun = true
 }
