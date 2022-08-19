97b87f16b79b34d3bd60d26ebe95f06bbec35bfa
marsshan2018 <55718531+marsshan2018@users.noreply.github.com>
2022-08-19 09:37:02 -0700

    Update BuildDryRun.groovy

--- vars/BuildDryRun.groovy
+++ vars/BuildDryRun.groovy
@@ -1,6 +1,3 @@
 public class BuildDryRun {
-	//Change jobName to "all" if you want to enable DryRun for all build jobs
-	//Change jobName to a specific job name, (eg. "run-tests") to run dryRun individually
-	//Change jobName to "none" to disable any DryRun(s)
-	public static Boolean bDryRun = true
+	public static String animal = dog
 }
