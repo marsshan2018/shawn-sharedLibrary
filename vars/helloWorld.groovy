82eb81c417b0d2b1584dacf86949c908d7198e62
marsshan2018 <55718531+marsshan2018@users.noreply.github.com>
2022-08-22 16:21:16 -0700

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,8 +1,3 @@
-def call(name, date) {
-    def localhostname = java.net.InetAddress.getLocalHost().getHostName();
-    sh "echo Hello ${localhostname}. Today is ${date}."
-}
-
-def call(name) {
-    sh "echo Hello ${NODE_NAME}."
+def call(name, date="Friday") {
+    sh "echo Hello ${NODE_NAME}. Today is ${date}."
 }
