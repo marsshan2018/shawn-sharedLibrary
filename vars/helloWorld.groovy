e22d8b9d07e092653d87bcbe73ec622efdc6b458
marsshan2018 <55718531+marsshan2018@users.noreply.github.com>
2022-08-22 16:24:37 -0700

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,4 @@
 def call(name, date="Friday") {
-    println "Hello ${name}. Today is {date}."
+    println "Hello ${name}. Today is ${date}."
     //sh "echo Hello ${NODE_NAME}. Today is ${date}."
 }
